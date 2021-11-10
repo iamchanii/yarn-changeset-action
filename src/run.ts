d07b25ddccc6bdb81b2cc443f9b857e6e6162d16
iamchanii <imch@dano.me>
2021-11-10 16:22:40 +0900

    add log for test

--- src/run.ts
+++ src/run.ts
@@ -99,6 +99,7 @@ export async function runPublish({
   let lines = changesetPublishOutput.stdout.split("\n");
   for (let line of lines) {
     let match = line.replace(/\\u001b[^m]*?m/g, "").match(publishedPattern);
+    console.log(match);
     if (match === null) {
       continue;
     }
